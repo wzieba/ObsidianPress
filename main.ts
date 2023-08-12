e7f63dcf776373b553b3b14b78026e1f0a6ebbc3
Wojciech Zięba <wojciech.zieba@protonmail.com>
2023-08-12 19:19:07 +0200

    feat: authenticate with WPCOM OAuth

--- main.ts
+++ main.ts
@@ -1,78 +1,61 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Plugin,
+	PluginSettingTab,
+	requestUrl,
+	Setting
+} from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface PluginSettings {
+	accessToken: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: PluginSettings = {
+	accessToken: ''
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export function generateQueryString(params: Record<string, undefined | number | string>): string {
+	return new URLSearchParams(
+		Object.fromEntries(
+			Object.entries(params).filter(([k, v]) => v !== undefined)
+		) as Record<string, string>
+	).toString();
+}
+
+export default class ObsidianPress extends Plugin {
+	settings: PluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.registerObsidianProtocolHandler("obsidianpress-plugin-oauth", async (data) => {
+			requestUrl({
+				url: "https://public-api.wordpress.com/oauth2/token",
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+					'User-Agent': 'obsidian.md'
+				},
+				body: generateQueryString({
+					grant_type: 'authorization_code',
+					client_id: "89477",
+					code: data.code,
+					redirect_uri: "obsidian://obsidianpress-plugin-oauth",
+				})
+			}).then(response => {
+				console.log('getToken response', response);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+				this.settings.accessToken = response.json.access_token
+				this.saveSettings()
+			}).catch(function (error) {
+				console.log(error.message);
+			});
+		})
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		this.addSettingTab(new SettingsTab(this.app, this));
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
@@ -91,26 +74,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class SettingsTab extends PluginSettingTab {
+	plugin: ObsidianPress;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ObsidianPress) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,15 +87,20 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
+		const authUrl = new URL("https://public-api.wordpress.com/oauth2/authorize")
+		authUrl.searchParams.append("client_id", "89477")
+		authUrl.searchParams.append("redirect_uri", "obsidian://obsidianpress-plugin-oauth")
+		authUrl.searchParams.append("response_type", "code")
+		authUrl.searchParams.append("blog", "wooengage.wordpress.com")
+		authUrl.searchParams.append("scope", "users")
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName('Authorize')
+			.addButton(button => button
+				.setButtonText("Authorize with WPCOM")
+				.onClick(() => {
+					window.open(authUrl.href);
+				})
+			)
 	}
 }
