5301f4e65f6160a56990ec95860d09f734e7a4ce
Wojciech Zięba <wojciech.zieba@protonmail.com>
2023-08-16 23:23:28 +0200

    feat: add name, username and avatar to suggestions^

--- MentionSuggest.ts
+++ MentionSuggest.ts
@@ -12,18 +12,24 @@ import ObsidianPress from "./main";
 
 export class WPUser {
 	userLogin: string;
+	name: string;
+	avatar: string;
 
-	constructor(userLogin: string) {
-		this.userLogin = userLogin;
+	constructor(username: string, name: string, avatar: string) {
+		this.userLogin = username;
+		this.name = name;
+		this.avatar = avatar;
 	}
 }
 
 export class MentionSuggest extends EditorSuggest<WPUser> {
 
 	users: WPUser[] = [];
+	plugin: ObsidianPress;
 
 	constructor(app: App, plugin: ObsidianPress) {
 		super(app);
+		this.plugin = plugin
 
 		requestUrl({
 			url: "https://public-api.wordpress.com/rest/v1.1/users/suggest?site_id=208157483",
@@ -33,13 +39,16 @@ export class MentionSuggest extends EditorSuggest<WPUser> {
 				'authorization': `Bearer ${plugin.settings.accessToken}`
 			}
 		}).then(response => {
-			this.users = response.json.suggestions.map((jsonUser) => new WPUser(jsonUser.user_login))
-			console.log(`Fetched users to suggest: ${this.users.map((it) => it.userLogin)}`)
+			this.users = response.json.suggestions.map((jsonUser) => new WPUser(
+				jsonUser.user_login,
+				jsonUser.display_name,
+				jsonUser.image_URL,
+			))
 		});
 	}
 
 	getSuggestions(context: EditorSuggestContext): WPUser[] | Promise<WPUser[]> {
-		return this.users.filter((user) => user.userLogin.includes(context.query))
+		return this.users.filter((user) => user.userLogin.includes(context.query) || user.name.includes(context.query))
 	}
 
 	onTrigger(cursor: EditorPosition, editor: Editor, file: TFile | null): EditorSuggestTriggerInfo | null {
@@ -62,10 +71,37 @@ export class MentionSuggest extends EditorSuggest<WPUser> {
 	}
 
 	renderSuggestion(value: WPUser, el: HTMLElement): void {
-		const text = el.doc.createElement("div");
-		text.addClass("completr-suggestion-text");
-		text.setText(value.userLogin);
-		el.appendChild(text);
+		const avatar = el.doc.createElement("img")
+		avatar.addClass("obsidianpress-suggestion-avatar")
+
+		requestUrl({
+			url: value.avatar + "&w=32",
+			method: 'GET',
+			headers: {
+				'authorization': `Bearer ${this.plugin.settings.accessToken}`,
+				'User-Agent': 'obsidian.md'
+			}
+		}).then(response => {
+
+			const arrayBuffer = response.arrayBuffer;
+			const bytes = new Uint8Array(arrayBuffer);
+			const blob = new Blob([bytes.buffer]);
+
+			const reader = new FileReader();
+			reader.onload = function (e) {
+				avatar.src = e.target.result;
+			};
+			reader.readAsDataURL(blob);
+		})
+
+		const nameSpan = el.doc.createElement("span");
+		nameSpan.addClass("obsidianpress-suggestion-name")
+		nameSpan.setText(value.name);
+		const usernameSpan = el.doc.createElement("small")
+		usernameSpan.addClass("obsidianpress-suggestion-username")
+		usernameSpan.setText(value.userLogin)
+		el.addClass("obsidianpress-suggestion-container")
+		el.append(...[avatar, nameSpan, usernameSpan])
 	}
 
 	selectSuggestion(value: WPUser, evt: MouseEvent | KeyboardEvent): void {
